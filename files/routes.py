e138435d1262099c52697c6a69db11d1fd1aad93
Linette V Maliakal <lmaliakal21@gmail.com>
2019-05-01 19:46:48 -0500

    yeet

--- files/routes.py
+++ files/routes.py
@@ -74,7 +74,7 @@ def save_picture(form_picture):
     _, f_ext = os.path.splitext(form_picture)
     picture_fn = random_hex + f_ext
     picture_path = os.path.join(app.root_path, 'static/posts', picture_fn)
-    output_size = (125, 125)
+    output_size = (175, 175)
     i = Image.open(form_picture)
     i.thumbnail(output_size)
     i.save(picture_path)
